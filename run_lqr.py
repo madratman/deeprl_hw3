ec8f60f68f4902dd7e0e6fd2905554f1d74c0777
Adithyavairavan Murali <amurali@GS16692.SP.CS.CMU.EDU>
2017-04-25 20:44:52 -0400

    Wrote code for plotting in LQR

--- run_lqr.py
+++ run_lqr.py
@@ -1,12 +1,14 @@
 import numpy as np
 import gym
-# from IPython import embed
+from IPython import embed
 import deeprl_hw3.arm_env
 from deeprl_hw3.controllers import calc_lqr_input
 import copy
 import time
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
-env_name = 'TwoLinkArm-v0'
+env_name = 'TwoLinkArm-limited-torque-v1'
 env = gym.make(env_name)
 sim_env = gym.make(env_name)
 initial_state = env.reset()
@@ -15,7 +17,12 @@ env.render()
 total_reward = 0
 num_steps = 0
 
+Q = []
+U = []
+traj_iter = 0
+
 while True:
+  traj_iter += 1
   if num_steps >= 0:
     u = calc_lqr_input(env, sim_env)
   else:
@@ -30,5 +37,37 @@ while True:
   total_reward += reward
   num_steps += 1
 
+  Q.append(env.state)
+  U.append(u)
+
   if is_terminal:
   	break
+
+Q = np.array(Q)
+U = np.array(U)
+
+plt.subplot(311)
+plt.xlabel('t')
+plt.ylabel('Control u')
+plt.title('LQR Solution for ' + env_name)
+blue_patch = mpatches.Patch(color='blue', label='Control 0')
+red_patch = mpatches.Patch(color='red', label='Control 1')
+plt.legend(handles=[red_patch, blue_patch])
+plt.plot(range(traj_iter), U[:,0], 'b', range(traj_iter), U[:,1], 'r')
+
+plt.subplot(312)
+plt.xlabel('t')
+plt.ylabel('q/position')
+blue_patch = mpatches.Patch(color='blue', label='q 0')
+red_patch = mpatches.Patch(color='red', label='q 1')
+plt.legend(handles=[red_patch, blue_patch])
+plt.plot(range(traj_iter), Q[:,0], 'b', range(traj_iter), Q[:,1], 'r')
+
+plt.subplot(313)
+plt.xlabel('t')
+plt.ylabel('q_dot/velocity')
+blue_patch = mpatches.Patch(color='blue', label='q_dot 0')
+red_patch = mpatches.Patch(color='red', label='q_dot 1')
+plt.legend(handles=[red_patch, blue_patch])
+plt.plot(range(traj_iter), Q[:,2], 'b', range(traj_iter), Q[:,3], 'r')
+plt.show()
