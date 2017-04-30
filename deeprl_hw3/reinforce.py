81ee84bd6d35e4c502eebbb4f43ef3bbdfc22a29
Alex Spitzer <aes368@cornell.edu>
2017-04-30 02:54:37 -0400

    Finalize reinforce algo, add evaluation, and add data to report

--- deeprl_hw3/reinforce.py
+++ deeprl_hw3/reinforce.py
@@ -5,93 +5,59 @@ import tensorflow as tf
 
 from imitation import load_model
 
-def get_total_reward(env, model):
-    """compute total reward
-
-    Parameters
-    ----------
-    env: gym.core.Env
-      The environment.
-    model: (your action model, which can be anything)
-
-    Returns
-    -------
-    total_reward: float
-    """
-    return 0.0
-
-
 def choose_action(model, observation, sess):
-    """choose the action
-
-    Parameters
-    ----------
-    model: (your action model, which can be anything)
-    observation: given observation
-
-    Returns
-    -------
-    p: float
-        probability of action 1
-    action: int
-        the action you choose
-    """
     output = sess.run(model.output, feed_dict={model.input : np.array([observation])})[0]
-    return output[1], np.random.uniform() > output[0]
+    return np.random.uniform() > output[0]
 
+def run_episode(env, model, sess):
+  state = env.reset()
+
+  # List of state, action, reward tuples.
+  episode = []
+
+  while 1:
+    action = choose_action(model, state, sess)
+    next_state, reward, done, info = env.step(action)
+
+    episode.append((state, action, reward))
+    state = next_state
+
+    if done:
+      break
+
+  return episode
 
 def reinforce(env, model):
-    """Policy gradient algorithm
+  action_taken = tf.placeholder(tf.int32, shape=(), name='action_taken')
+  G_t_var = tf.placeholder(tf.float32, shape=(), name='G_t')
 
-    Parameters
-    ----------
-    env: your environment
+  relevant_pi = tf.gather(tf.transpose(model.output), action_taken)
+  update_op = tf.train.AdamOptimizer(ALPHA).minimize(-G_t_var * tf.log(relevant_pi))
 
-    Returns
-    -------
-    total_reward: float
-    """
-    ALPHA = 1e-4 # SGD Learning rate
+  sess = tf.Session()
+  sess.run(tf.global_variables_initializer())
 
-    action_taken = tf.placeholder(tf.int32, shape=(), name='action_taken')
-    G_t_var = tf.placeholder(tf.float32, shape=(), name='G_t')
+  # Avg, min, max triples (over N training episodes)
+  learning_curve = []
 
-    relevant_pi = tf.gather(tf.transpose(model.output), action_taken)
-    update_op = tf.train.AdamOptimizer(ALPHA).minimize(-G_t_var * tf.log(relevant_pi))
+  for episode_no in range(1, NUM_EPISODES + 1):
+    G_t = 0 # The return
+    for state, action, reward in run_episode(env, model, sess)[::-1]:
+      G_t += reward
+      feed_dict = { model.input : np.array([state]), action_taken : action, G_t_var : G_t }
+      sess.run(update_op, feed_dict=feed_dict)
 
-    sess = tf.Session()
-    sess.run(tf.global_variables_initializer())
+    if not episode_no % EVAL_EVERY:
+      print(episode_no, ": evaluating for 100 episodes...", end='')
+      rewards = [len(run_episode(env, model, sess)) for i in range(100)]
+      learning_curve.append((np.mean(rewards), np.min(rewards), np.max(rewards)))
+      print(learning_curve[-1])
 
-    state = env.reset()
+  return learning_curve
 
-    # List of state, action, reward tuples.
-    episode = []
-
-    rewards = []
-    episode_no = 1
-    while episode_no < 800:
-      prob, action = choose_action(model, state, sess)
-      next_state, reward, done, info = env.step(action)
-
-      episode.append((state, action, reward))
-      state = next_state
-
-      if done:
-        episode_no += 1
-        print(len(episode))
-        rewards.append(len(episode))
-
-        # Train
-        G_t = 0 # The return
-        for state, action, reward in episode[::-1]:
-          G_t += reward
-          feed_dict = { model.input : np.array([state]), action_taken : action, G_t_var : G_t }
-          sess.run(update_op, feed_dict=feed_dict)
-
-        state = env.reset()
-        episode = []
-
-    return rewards
+ALPHA = 2.5e-4 # SGD Learning rate
+EVAL_EVERY = 20
+NUM_EPISODES = 800
 
 if __name__ == "__main__":
   import os
@@ -103,7 +69,11 @@ if __name__ == "__main__":
   config_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "CartPole-v0_config.yaml")
   model = load_model(config_path)
 
-  rewards = reinforce(env, model)
+  avg, mins, maxs = map(np.array, zip(*reinforce(env, model)))
 
-  pyplot.plot(rewards)
+  pyplot.errorbar(EVAL_EVERY * np.array(range(len(avg))), avg, yerr=[avg - mins, maxs - avg], ecolor='red', capsize=3)
+  pyplot.xlabel("Training Episode No.")
+  pyplot.ylabel("Average Reward (100 Episodes)")
+  pyplot.title("Average Reward During Training")
+  pyplot.savefig("reinforce_plot.pdf", transparent=True)
   pyplot.show()
