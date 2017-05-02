abd6261056e46d3816793a2147a54066719d2645
shohinm <shohin.93@gmail.com>
2017-05-02 16:54:47 -0400

    debugging

--- deeprl_hw3/imitation.py
+++ deeprl_hw3/imitation.py
@@ -32,6 +32,65 @@ def load_model(model_config_path, model_weights_path=None):
     return model
 
 
+# def generate_expert_training_data(expert, env, num_episodes=100, render=True):
+#     """Generate training dataset.
+
+#     Parameters
+#     ----------
+#     expert: keras.models.Model
+#       Model with expert weights.
+#     env: gym.core.Env
+#       The gym environment associated with this expert.
+#     num_episodes: int, optional
+#       How many expert episodes should be run.
+#     render: bool, optional
+#       If present, render the environment, and put a slight pause after
+#       each action.
+
+#     Returns
+#     -------
+#     expert_dataset: ndarray(states), ndarray(actions)
+#       Returns two lists. The first contains all of the states. The
+#       second contains a one-hot encoding of all of the actions chosen
+#       by the expert for those states.
+#     """
+
+#     states_arr = np.empty((0, 4))
+#     actions_arr = np.empty((0, 2))
+
+#     for i in range(num_episodes):
+#         print('generate_expert_training_data :: episode {}'.format(i))
+#         state = env.reset()
+#         state = np.reshape(state, (1, state.shape[0]))
+#         if render:
+#             env.render()
+#             time.sleep(.1)
+#         is_done = False
+#         while not is_done:
+#             # print(state.shape)
+#             # print(state.shape[0])
+#             # print(np.reshape(state, (1, state.shape[0])).shape)
+
+#             # action = np.argmax(expert.predict_on_batch(state[np.newaxis, ...])[0])
+#             action = np.argmax(expert.predict(state, batch_size=1)[0,:])
+#             # print("action", action)
+#             if action == 0:
+#                 action_one_hot = np.array([[0., 1.]])
+#             else:
+#                 action_one_hot = np.array([[1., 0.]])
+#             next_state, reward, is_done, _ = env.step(action)
+#             next_state = np.reshape(next_state, (1, next_state.shape[0]))
+#             states_arr = np.append(states_arr, state, axis=0)
+#             actions_arr = np.append(actions_arr, action_one_hot, axis=0)
+#             # print(state.shape, states_arr.shape)
+#             # print(action_one_hot.shape, actions_arr.shape)
+#             state = next_state
+#             if render:
+#                 env.render()
+#                 time.sleep(.1)
+
+#     print(' \n DONE generate_expert_training_data \n')
+#     return states_arr, actions_arr
 def generate_expert_training_data(expert, env, num_episodes=100, render=True):
     """Generate training dataset.
 
@@ -55,41 +114,31 @@ def generate_expert_training_data(expert, env, num_episodes=100, render=True):
       by the expert for those states.
     """
 
-    states_arr = np.empty((0, 4))
+    states_arr = np.empty((0,4))
     actions_arr = np.empty((0, 2))
 
     for i in range(num_episodes):
-        print('generate_expert_training_data :: episode {}'.format(i))
-        state = env.reset()
-        state = np.reshape(state, (1, state.shape[0]))
+        currState = env.reset()
         if render:
             env.render()
-            time.sleep(.1)
-        is_done = False
-        while not is_done:
-            # print(state.shape)
-            # print(state.shape[0])
-            # print(np.reshape(state, (1, state.shape[0])).shape)
-
-            # action = np.argmax(expert.predict_on_batch(state[np.newaxis, ...])[0])
-            action = np.argmax(expert.predict(state, batch_size=1))
-            # print("action", action)
-            if action == 0:
-                action_one_hot = np.array([[0., 1.]])
-            else:
-                action_one_hot = np.array([[1., 0.]])
-            state, reward, is_done, _ = env.step(action)
-            state = np.reshape(state, (1, state.shape[0]))
-            states_arr = np.append(states_arr, state, axis=0)
-            actions_arr = np.append(actions_arr, action_one_hot, axis=0)
-            # print(state.shape, states_arr.shape)
-            # print(action_one_hot.shape, actions_arr.shape)
+        currState = np.reshape(currState, (1,currState.size))
+        is_terminal = False
+        while is_terminal == False:
+            actionP = expert.predict(currState, batch_size = 1)
+            action = np.argmax(actionP[0,:])
+            nextState, reward, is_terminal, _ = env.step(action)
+            nextState = np.reshape(nextState, (1,nextState.size))
             if render:
-                env.render()
-                time.sleep(.1)
+                env.render()       
+            action_oh = np.zeros((1,2))
+            np.put(action_oh,action,1)        
+            states = np.append(states, currState, axis = 0)
+            actions = np.append(actions,action_oh, axis = 0)
+            
+            currState = nextState 
+        # time.sleep(0.1)
 
-    print(' \n DONE generate_expert_training_data \n')
-    return states_arr, actions_arr
+    return states, actions
 
 def test_cloned_policy(env, cloned_policy, num_episodes=50, render=True):
     """Run cloned policy and collect statistics on performance.
